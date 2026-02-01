40380f5411ddec97fc7a20ae6d1e7a36a9123888
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 21:00:14 +0530

    logs

--- 22-log-install.sh
+++ 22-log-install.sh
@@ -8,38 +8,45 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_NAME=$( basename "$0" .sh )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)"
+
+echo "=======================" &>>"$LOG_FILE"
+echo "Script started executed at: $(date)" &>>"$LOG_FILE"
 
 if [ $USER_ID -ne 0 ]; then 
-   echo -e "ERROR: use root access" 
+   echo -e "ERROR: use root access"
+   echo -e "ERROR: use root access" &>>"$LOG_FILE"
    exit 1
 fi
 
 VALIDATE() {
-    if [ $1 -ne 0 ]; then
-       echo -e "Error: $2... $R installation is failed $N" 
+    if [ $1 -ne 0 ]; then 
+       echo -e "Error: $2... $R installation is failed $N"
+       echo -e "Error: $2... $R installation is failed $N" &>>"$LOG_FILE"
        exit 1
     else 
-       echo -e " $2.. $G installation is successfull $N" 
+       echo -e " $2.. $G installation is successfull $N"
+       echo -e " $2.. $G installation is successfull $N" &>>"$LOG_FILE"
     fi
 }
 
 dnf list installed mysql
     if [ $? -ne 0 ]; then
-       dnf install mysql -y
+       dnf install mysql -y &>$LOG_FILE
        VALIDATE $? "MYSQL"
     else
+       echo -e "Already mysql is installed... $Y Skipping $N"
        echo -e "Already mysql is installed... $Y Skipping $N" &>$LOG_FILE
     fi
 
 dnf list installed nginx
     if [ $? -ne 0 ]; then 
-        dnf install nginx -y
+        dnf install nginx -y &>$LOG_FILE
         VALIDATE $? "NGINX"
     else 
+        echo -e "NGNX already installed... $Y skipping $N"
         echo -e "NGNX already installed... $Y skipping $N" &>$LOG_FILE
     fi
