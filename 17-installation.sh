4632e141efaec348846a3b13674458a09d7e3a43
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 19:15:36 +0530

    installation

--- 17-installation.sh
+++ 17-installation.sh
@@ -4,12 +4,14 @@ USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
    echo "ERROR: Please use root access to install"
+   exit 1;
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
    echo "ERROR: installation is failure"
+   exit 1;
 else
    echo "Installation is successful"
 fi
