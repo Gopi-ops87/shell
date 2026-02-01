425feb7a8bebd5a469ba2febdcbd25a5babea4d8
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 19:46:21 +0530

    color

--- 19-color.sh
+++ 19-color.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "\e[31m Hello World"
-echo -e "\e[32m Hello World"
-echo -e "\e[33m Hello World"
-echo -e "\e[34m Hello World"
-echo -e "\e[35m Hello World"
-echo -e "\e[36m Hello World"
-echo -e "\e[37m Hello World"
+echo -e "\e[31m Hello World"  # RED
+echo -e "\e[32m Hello World"  # GREEN
+echo -e "\e[33m Hello World"  # YELLOW
+echo -e "\e[34m Hello World"  # BLUE
+echo -e "\e[35m Hello World"  # PURPLE
+echo -e "\e[36m Hello World"  # SIEN
+echo -e "\e[37m Hello World"  # WHITE
