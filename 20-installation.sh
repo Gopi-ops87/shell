7bfd10fe96e848183d464ce0d8211162282fd61e
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 19:57:29 +0530

    color

--- 20-installation.sh
+++ 20-installation.sh
@@ -3,21 +3,21 @@
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="e[37m"
+N="e[0m"
 
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then 
-   echo -e "ERROR: $R use root access $N"
+   echo -e "ERROR: ${R} use root access ${N}"
    exit 1
 fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-       echo -e "Error: $R $2 installation is failed $N"
+       echo -e "Error: ${R} $2 installation is failed ${N}"
        exit 1
     else 
-       echo -e "$G $2 installation is successfull $N"
+       echo -e "${G} $2 installation is successfull ${N}"
     fi
 }
 
