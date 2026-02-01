fd81ac4e3ada0a04949a536475a016675774f379
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 16:18:21 +0530

    special

---
+++ 08-spe-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "current script name is: $0"
+echo "first argument is: $1"
+echo "second argument is: $2"
+echo "number of args is: $#"
+echo "all args seperately : $@"
+echo "all arguments as one string: $*"
+echo "PID of current script: $$"
+echo "exit status of last command: $?"
+echo "PID of last bacground command: $0"
