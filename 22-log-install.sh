d0857f038ae879883241e8c1c71bf90d408ef73b
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 20:46:00 +0530

    logs

--- 22-log-install.sh
+++ 22-log-install.sh
@@ -15,16 +15,16 @@ mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)"
 
 if [ $USER_ID -ne 0 ]; then 
-   echo -e "ERROR: use root access" &>>$LOG_FILE
+   echo -e "ERROR: use root access" &>$LOG_FILE
    exit 1
 fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-       echo -e "Error: $2... $R installation is failed $N" &>>$LOG_FILE
+       echo -e "Error: $2... $R installation is failed $N" &>$LOG_FILE
        exit 1
     else 
-       echo -e " $2.. $G installation is successfull $N" &>>$LOG_FILE
+       echo -e " $2.. $G installation is successfull $N" &>$LOG_FILE
     fi
 }
 
@@ -33,7 +33,7 @@ dnf list installed mysql
        dnf install mysql -y
        VALIDATE $? "MYSQL"
     else
-       echo -e "Already mysql is installed... $Y Skipping $N" &>>$LOG_FILE
+       echo -e "Already mysql is installed... $Y Skipping $N" &>$LOG_FILE
     fi
 
 dnf list installed nginx
@@ -41,5 +41,5 @@ dnf list installed nginx
         dnf install nginx -y
         VALIDATE $? "NGINX"
     else 
-        echo -e "NGNX already installed... $Y skipping $N" &>>$LOG_FILE
+        echo -e "NGNX already installed... $Y skipping $N" &>$LOG_FILE
     fi
