5539883242f4ab94fbbdffe005ad4aaf049f3e34
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-01 20:18:01 +0530

    color

---
+++ 21-installation.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USER_ID -ne 0 ]; then 
+   echo -e "ERROR: use root access"
+   exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+       echo -e "Error: $2... $R installation is failed $N"
+       exit 1
+    else 
+       echo -e " $2.. $G installation is successfull $N"
+    fi
+}
+
+dnf list installed mysql
+    if [ $? -ne 0 ]; then
+       dnf install mysql -y
+    else
+       echo "Already mysql is installed... $Y Skipping $N"
+    fi
+VALIDATE $? "MYSQL"
+
+dnf list installed nginx
+     if [ $? -ne 0 ]; then 
+        dnf install nginx -y
+     else 
+        echo "NGNX already installed... $Y skipping $N"
+     fi
+VALIDATE $? "NGINX"
