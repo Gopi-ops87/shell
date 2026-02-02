ee3c4b0ff8d4293a8413728e67a30715642ce1f3
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-02 17:14:20 +0530

    install

---
+++ 25-loops.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( basename "$0" .sh)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "======================" &>>"$LOG_FILE"
+echo "script started executed at: $(date)" &>>"$LOG_FILE"
+
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR:: please use root access"
+    echo "ERROR:: please use root access" &>>"$LOG_FILE"
+    exit 1
+fi
+
+VALIDATE() {
+            if [ $1 -ne 0 ]; then
+                echo -e "ERROR:: $2 ....$R installation is failed $N"
+                echo -e "ERROR:: $2 ....$R installation is failed $N" &>>"$LOG_FILE"
+                exit 1
+            else
+                echo -e "$2.. $G Installaton is success $N"
+                echo -e "$2.. $G Installaton is success $N" &>>"$LOG_FILE"
+            fi
+}
+
+for package in $@
+do
+    echo "My packega is: $package"
+done
